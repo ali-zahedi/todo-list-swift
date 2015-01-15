44481e2a4b15750c74da977de44abea63ea79cfa
Prateek Bhatt <prattbhatt@gmail.com>
2015-01-15 14:21:06 +0530

    added methods to add and delete to do items from the list; also persistent storage across app sessions using NSUserDefaults

--- To Do List/FirstViewController.swift
+++ To Do List/FirstViewController.swift
@@ -8,8 +8,12 @@
 
 import UIKit
 
-class FirstViewController: UIViewController {
+var toDoItems: [String] = []
 
+class FirstViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+
+    @IBOutlet weak var tasksTable: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -18,6 +22,53 @@ class FirstViewController: UIViewController {
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        
+        return toDoItems.count
+        
+    }
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        
+        var cell = UITableViewCell(style: UITableViewCellStyle.Default, reuseIdentifier: "Cell")
+        cell.textLabel?.text = toDoItems[indexPath.row]
+        
+        return cell
+
+        
+    }
+    
+    override func viewWillAppear(animated: Bool) {
+        
+        
+        if var storedToDoItems: AnyObject = NSUserDefaults.standardUserDefaults().objectForKey("toDoItems") {
+            
+            toDoItems = []
+            
+            for var i = 0; i < storedToDoItems.count; ++i {
+                toDoItems.append(storedToDoItems[i] as NSString)
+            }
+            
+        }
+    }
+    
+    func tableView(tableView: UITableView, commitEditingStyle editingStyle: UITableViewCellEditingStyle, forRowAtIndexPath indexPath: NSIndexPath) {
+        
+        if editingStyle == UITableViewCellEditingStyle.Delete {
+            
+            toDoItems.removeAtIndex(indexPath.row)
+            
+            NSUserDefaults.standardUserDefaults().setObject(toDoItems, forKey: "toDoItems")
+            
+            NSUserDefaults.standardUserDefaults().synchronize()
+            
+            
+            tasksTable.reloadData()
+            
+        }
+        
     }
 
 
--- To Do List/SecondViewController.swift
+++ To Do List/SecondViewController.swift
@@ -8,8 +8,24 @@
 
 import UIKit
 
-class SecondViewController: UIViewController {
+class SecondViewController: UIViewController, UITextFieldDelegate {
+    
+    @IBOutlet weak var newToDoItem: UITextField!
+    
+    @IBAction func addItem(sender: AnyObject) {
+        
+        toDoItems.append(newToDoItem.text)
+        
+        self.view.endEditing(true)
+        
+        newToDoItem.text = ""
+        
+        NSUserDefaults.standardUserDefaults().setObject(toDoItems, forKey: "toDoItems")
+        
+        NSUserDefaults.standardUserDefaults().synchronize()
 
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +36,14 @@ class SecondViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    func textFieldShouldReturn(textField: UITextField) -> Bool {
+        textField.resignFirstResponder()
+        return true
+    }
+    
+    override func touchesBegan(touches: NSSet, withEvent event: UIEvent) {
+        self.view.endEditing(true)
+    }
 
 }
 
